d2232445ba7ff2b757f1f0e539d3db98ae34ce37
Tejan Karmali <tejank10@gmail.com>
2019-06-07 15:06:47 +0530

    fixed array bug

--- src/objects.jl
+++ src/objects.jl
@@ -97,7 +97,7 @@ function render(obj::AbstractWorldObj, draw_bbox)
 end
 
 function triangulate_faces(list_verts::Matrix, color::Vec3)
-    v1 = Vec3(list_verts[1, 1:1], -list_verts[1, 2:2], [list_verts[1, 3:3]])
+    v1 = Vec3(list_verts[1, 1:1], -list_verts[1, 2:2], list_verts[1, 3:3])
     Δs = []
     for i in 2:size(list_verts, 1)-1
         v2 = Vec3(list_verts[i, 1:1], -list_verts[i, 2:2], list_verts[i, 3:3])
