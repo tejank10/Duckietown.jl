74ad3e3c9a24b834fc9464f361a484686718d9e1
Tejan Karmali <tejank10@gmail.com>
2019-06-10 11:41:09 +0530

    shifted comment

--- src/simulator.jl
+++ src/simulator.jl
@@ -1174,7 +1174,7 @@ function _render_img(sim::Simulator, top_down=true)
     #gl.glEnable(gl.GL_TEXTURE_2D)
     #gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
     #gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-    #=
+    
     # For each grid tile
     for j in 1:sim._map._grid.grid_height
         for i in 1:sim._map._grid.grid_width
@@ -1220,7 +1220,7 @@ function _render_img(sim::Simulator, top_down=true)
             end
         end
     end
-    
+    #=
     # For each object
     for (idx, obj) in enumerate(_objects(sim))
         #TODO: put in scene
