83549fdac9349654c3005e3135547ab69b147390
Tejan Karmali <tejank10@gmail.com>
2019-06-10 12:49:58 +0530

    Update simulator.jl

--- src/simulator.jl
+++ src/simulator.jl
@@ -1176,14 +1176,14 @@ function _render_img(sim::Simulator, top_down=true)
     #gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
     
     # For each grid tile
-    #=
+   #=
     for j in 1:sim._map._grid.grid_height
         for i in 1:sim._map._grid.grid_width
             # Get the tile type and angle
             tile = _get_tile(_grid(sim), i, j)
             
             (ismissing(tile) || isnothing(tile)) && continue
-            #=
+          
             # kind = tile['kind']
             angle = tile["angle"]
             color = tile["color"]
@@ -1222,8 +1222,7 @@ function _render_img(sim::Simulator, top_down=true)
             end
         end
     end
-    =#
-    #=
+
     # For each object
     for obj in _objects(sim)
         #TODO: put in scene
