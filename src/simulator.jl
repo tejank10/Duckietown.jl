3d63cf3085473aabf4c5287874019b8c3c4d2f49
Tejan Karmali <tejank10@gmail.com>
2019-06-11 12:15:35 +0530

    Update simulator.jl

--- src/simulator.jl
+++ src/simulator.jl
@@ -1230,7 +1230,8 @@ function _render_img(sim::Simulator, top_down=true)
                         
     # For each grid tile
     len = sim._map._grid.grid_height * sim._map._grid.grid_width - 1
-    tri_vecs = map(j->fn(j), collect(0:len))
+    range = collect(0:len)
+    tri_vecs = map(j->fn(j), range)
     tri_vecs_filtered = filter(tri_vec->!isnothing(tri_vec), tri_vecs)
     scene = vcat(scene, tri_vecs_filtered...)
     
